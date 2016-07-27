6d9cb258c33eb5f33fa11bfd96b8442d78adf251
hanmhs9 <hanmhs9@gmail.com>
2016-07-27 16:34:36 -0400

    Connecting to browser

---
+++ ReadFileMain.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class ReadFileMain {
+	public static void main(String[] args){
+		ReadFile1 dude = new ReadFile1();
+		dude.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+}
